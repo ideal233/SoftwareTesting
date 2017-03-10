a4705544c24bddf86acf1903b13b8b34e2b203a5
ideal233 <767078184@qq.com>
2017-03-10 20:10:39 +0800

    lab 1

--- Lab1/Lab1/test/st/lab1/TriangleTest.java
+++ Lab1/Lab1/test/st/lab1/TriangleTest.java
@@ -3,17 +3,63 @@ package st.lab1;
 import static org.junit.Assert.*;
 
 import org.junit.Before;
+import org.junit.After;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
 public class TriangleTest {
+	
+	private int a;
+	private int b;
+	private int c;
+	private String result;
+	private static int count = 1;
+	private Triangle tri;
+	
+	public TriangleTest(int a, int b, int c, String result)
+	{
+		this.a = a;
+		this.b = b;
+		this.c = c;
+		this.result = result;
+	}
+	
+	@Parameters
+	public static Collection data()
+	{
+		return Arrays.asList(new Object[][]{
+			{1, 1, 2, "not a triangle"},
+			{1, 1, 1, "equilateral"},
+			{2, 2, 3, "isosceles"},
+			{3, 4, 5, "scalene"}
+		});
+	}
 
-	Triangle tri = new Triangle();
+	@Before
+	public void setUp() throws Exception 
+	{
+		System.out.println("Create a triangle " + count);
+		tri = new Triangle(a, b, c);
+	}
 
 	@Test
-	public void testTriangle() {
-		assertEquals("equilateral", tri.triangle(1, 1, 1));
-		assertEquals("isosceles",tri.triangle(2, 2, 3));
-		assertEquals("scalene",tri.triangle(3, 4, 5));
+	public void test() {
+		System.out.println("Processing test" + count);
+		assertEquals(result, tri.triangle());
 	}
+	
+	@After
+	public void tearDown() throws Exception
+	{
+		count++;
+		System.out.println("test " + (count - 1) + " have finished!");
+	}
+	
 
 }
